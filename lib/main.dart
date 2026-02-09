9a3fea68ebb776851d0b50efb453a8bcc63bbb44
dianakius <eingidiana@gmail.com>
2026-02-09 22:02:59 +0200

    feat: create mew c;ass Sandbox

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,38 @@ import 'package:coffee_card/home.dart';
 void main() {
   runApp(const MaterialApp(home: Home()));
 }
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: 100,
+            color: Colors.green,
+            child: const Text('one'),
+          ),
+          Container(
+            height: 200,
+            color: Colors.red,
+            child: const Text('two'),
+          ),
+          Container(
+            height: 300,
+            color: Colors.yellow,
+            child: const Text('three'),
+          ),
+        ]
+      )
+    );
+  }
+}
